1b6d2611f908ba4fce830997f18032a9cd5461fe
Andrey Savaskov <ultrazokk@gmail.com>
2023-11-28 19:27:20 +0300

    Выполнил задачу два(t2)

---
+++ t2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив на 10 целых чисел. Напишите программу, 
+// которая определяет количество чётных чисел в массиве.
+
+    int[] numbers = new int[10]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int countEven = CountEvenNumbers(numbers);
+
+        // Выводим результат
+    Console.WriteLine("Сгенерированный массив:");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+         Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Количество четных чисел в массиве: {countEven}");
+    
+    static int CountEvenNumbers(int[] arr)
+    {
+        int count = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % 2 == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
